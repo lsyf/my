47d11027bf94ec074ff4d76e8913bbd547dcf25f
Administrator <loushuiyifan@gmail.com>
2017-12-28 11:54:19 +0800

    通用异常处理

--- src/main/java/com/loushuiyifan/config/ExceptionAdvice.java
+++ src/main/java/com/loushuiyifan/config/ExceptionAdvice.java
@@ -1,15 +1,11 @@
 package com.loushuiyifan.config;
 
 import com.loushuiyifan.system.vo.JsonResult;
-import org.apache.shiro.authz.UnauthorizedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author 漏水亦凡
@@ -20,16 +16,13 @@ public class ExceptionAdvice {
     private Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
 
 
-    @ExceptionHandler({UnauthorizedException.class})
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ModelAndView processError(Exception e) {
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("title", "无权限访问");
-        mv.addObject("error", e);
-        mv.setViewName("error");
-        logger.error("error", e);
-        return mv;
-    }
+//    @ExceptionHandler({UnauthorizedException.class})
+//    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+//    @ResponseBody
+//    public JsonResult processError(Exception e) {
+//        logger.error("error", e);
+//        return JsonResult.failure("没有权限:"+e.getMessage());
+//    }
 
     @ExceptionHandler({Exception.class})
     @ResponseBody
